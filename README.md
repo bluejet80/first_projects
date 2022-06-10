3f390ef01a518ce1d1faccb0e09f6e78f121dd30
Jarett Young <know_jarett@yahoo.com>
2022-06-10 15:16:19 -0500

    updating the README file

--- README.md
+++ README.md
@@ -3,19 +3,43 @@
 ## List of Projects
 
 ### Imbed1
+
 This project show how to imbed webpages in other pages
 
 ### Mystify
+
 This project implemets the Mystify screen saver in the browser with javascript
 
 ### Node_js_tutor
+
 This is the notes from a Node Js Course
 
 ### Oreilly_book
+
 These are notes from a Javascript Oreilly Book
 
 ### dyn_prog
+
 These files are all about recursion
 
+#### Best Sum Program
+
+This program takes in an array of numbers and then from that tries to identify the best combination of the least amount of numbers that add up to the desired sum.
+
+Trying to think of a practical application of this program. Lets say you had so many people and you wanted them to be in groups of 3...
+
+Anyway, Lets look at what this program does.
+
+##### Parts of the Program
+
+With any recursion program the first thing is to identify the limits for the recusion. And this is the first thing that is done in the program.
+
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+#### Grid.js
+
 ### lesson_1
+
 More files...
--- dyn_prog/bestsum.js
+++ dyn_prog/bestsum.js
@@ -26,37 +26,35 @@ const bestSum = (targetSum, numbers) => {
 };
 */
 
-
 // memoized version
 
-const bestSum = (targetSum, numbers, memo={}) => {
-    if(targetSum in memo) return memo[targetSum];
-    if(targetSum === 0) return [];
-    if(targetSum < 0) return null;
+const bestSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
 
-    let shortestCombination = null;
+  let shortestCombination = null;
 
-    for(let num of numbers) {
-        const remainder = targetSum - num;
-        const remainderCombination = bestSum(remainder, numbers, memo);
-        if (remainderCombination !== null) {
-            const combination = [...remainderCombination, num ];
-            if (shortestCombination === null || combination.length < shortestCombination.length) {
-                shortestCombination = combination;
-            }
-
-        }
-
-
-
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const remainderCombination = bestSum(remainder, numbers, memo);
+    if (remainderCombination !== null) {
+      const combination = [...remainderCombination, num];
+      if (
+        shortestCombination === null ||
+        combination.length < shortestCombination.length
+      ) {
+        shortestCombination = combination;
+      }
     }
+  }
 
-    memo[targetSum] = shortestCombination;
-    return shortestCombination;
-
+  memo[targetSum] = shortestCombination;
+  return shortestCombination;
 };
 
 console.log(bestSum(7, [5, 3, 4, 7]));
 console.log(bestSum(8, [2, 3, 5]));
 console.log(bestSum(8, [1, 4, 5]));
 console.log(bestSum(100, [1, 2, 5, 25]));
+console.log(bestSum(34, [3, 1]));
